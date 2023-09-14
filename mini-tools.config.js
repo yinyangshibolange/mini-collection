5f1584d8922d783843cbc759703105c1b2a2769e
Zhang que <112451217+copilotuser1@users.noreply.github.com>
2023-09-14 11:21:15 +0800

    init

---
+++ mini-tools.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+ srcFold: 'static_dev', // 源文件夹
+ targetFold: 'static', // 构建到目标文件夹
+ hot: true, // 热监听
+ loaders: [
+     {
+         fold: 'css', // fold为./ 则遍历整个srcFold文件夹
+         use: 'postcss',
+         ext: 'css'
+     },
+     {
+         fold: 'images',
+         use: 'imagemin',
+         // tfold: 'images'
+         // ext: 'TTF', 根据压缩代码
+     },
+     {
+         fold: 'js',
+         // tfold: 'js',
+         use: 'uglifyJs',
+         ext: 'js'
+     },
+     {
+         file: 'icon.ico', // 直接拷贝单个文件
+         rename: 'favicon.ico',
+     }
+ ]
+}
